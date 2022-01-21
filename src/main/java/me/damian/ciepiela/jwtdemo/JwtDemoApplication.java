e98d7872067f5f98c7742a35620fd543a79699c8
Damian Ciepiela <dciepiela@yahoo.com>
2022-01-21 14:48:14 +0100

    init: base app with mongodb connection

---
+++ src/main/java/me/damian/ciepiela/jwtdemo/JwtDemoApplication.java
@@ -0,0 +1,13 @@
+package me.damian.ciepiela.jwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/damian/ciepiela/jwtdemo/JwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.damian.ciepiela.jwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
